88bc6015658b9783785461db95f02f3c9df869e9
ledong17 <ledong5012@gmail.com>
2022-08-03 17:29:21 +0900

    upgrade shopping list

--- shopping_list/js/main.js
+++ shopping_list/js/main.js
@@ -1,34 +1,81 @@
-const input = document.querySelector("#main .inputBox .input");
-const shoppingList = document.querySelector("#main .shoppingList");
-const shoopingListAll = document.querySelectorAll("#main .shoppingList li");
-const enterBtn = document.querySelector("#main .inputBox .enterBtn");
+const input = document.querySelector("#footer .inputBox .input");
+const shoppingList = document.querySelector(".shoppingList");
+const enterBtn = document.querySelector("#footer .inputBox .enterBtn");
 const binIcon = document.querySelectorAll("#main .shoppingList li .binIcon");
-const clearAll = document.querySelector("#main .clearBtnBox .clearAllBtn");
 
-input.addEventListener("keydown", (e) => {
+// DOM에 변동이 많을 경우에는 innerHTML 보다는 createElement를 활용
+// 주석은 함수 단위를 어떤 것을 하는 것인지를 명시한느 것이 좋음
+// 왜? 를 설명할 수 있어야함.
+
+function onAdd() {
+  const text = input.value;
+  // make and add new items
+  if (input.value === "" || input.value === " ") {
+    input.classList.add("error");
+    input.focus();
+  } else {
+    input.classList.remove("error");
+    const item = createItems(text);
+    shoppingList.appendChild(item);
+    item.scrollIntoView({ block: "center" });
+    input.value = "";
+    input.focus();
+  }
+}
+
+function createItems(text) {
+  const itemRow = document.createElement("li");
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+
+  const p = document.createElement("p");
+  p.innerText = text;
+
+  const span = document.createElement("span");
+  span.setAttribute("class", "binIcon fa-solid fa-trash-can");
+  span.addEventListener("click", () => {
+    shoppingList.removeChild(itemRow);
+  });
+
+  itemRow.appendChild(input);
+  itemRow.appendChild(p);
+  itemRow.appendChild(span);
+
+  return itemRow;
+}
+
+enterBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    // console.log(input.value)
-    if (input.value === "" || input.value === " ") {
-      input.classList.add("error");
-    } else {
-      input.classList.remove("error");
-      shoppingList.innerHTML += `
-            <li>
-              <input type="checkbox" name="" id="" />
-                <p>${input.value}</p>
-              <span class="binIcon material-icons">delete_outline</span>
-            </li>`;
-      input.value = null;
-    }
+    onAdd();
   }
 });
 
-// remove list
-binIcon.forEach((e) => {
-  e.addEventListener("click", () => e.parentNode.remove());
-});
+// 강의 듣기전 작성 코드
 
-// clear all
-shoopingListAll.forEach((e) => {
-  clearAll.addEventListener("click", () => e.remove());
-});
+// input.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") {
+//     // console.log(input.value)
+//     if (input.value === "" || input.value === " ") {
+//       input.classList.add("error");
+//     } else {
+//       input.classList.remove("error");
+//       shoppingList.innerHTML += `
+//             <li>
+//               <input type="checkbox" name="" id="" />
+//                 <p>${input.value}</p>
+//               <span class="binIcon material-icons">delete_outline</span>
+//             </li>`;
+//       input.value = null;
+//     }
+//   }
+// });
+
+// // remove list
+// binIcon.forEach((e) => {
+//   e.addEventListener("click", () => e.parentNode.remove());
+// });
